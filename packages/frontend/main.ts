e43cd0dadb1f366e3d276846463eb92ab6c8907f
Renat Gubaev <>
2020-05-04 19:37:03 +0300

    no message

--- packages/frontend/main.ts
+++
@@ -1,6 +0,0 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './src/AppModule';
-
-enableProdMode();
-platformBrowserDynamic().bootstrapModule(AppModule);
